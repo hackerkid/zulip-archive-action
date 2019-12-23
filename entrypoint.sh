9acfed74ae165c0e7c61c120289c8bbb67a1ac0b
Vishnu KS <yo@vishnuks.com>
2019-12-23 17:00:52 +0530

    Use custom docker image.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,7 @@ echo "Hello $1"
 echo "$(ls)"
 echo "Hello again"
 time=$(date)
-apt-get update -y
-apt-get install curl -y
-apt-get install python3.7-dev -y
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python3 get-pip.py --user
-pip3 install virtualenv
-virtualenv -p python3.7 .
+virtualenv -p python3 .
 source bin/activate
 pip3 install zulip
 echo ::set-output name=time::$time
