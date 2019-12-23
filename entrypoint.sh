656905d56ae755b9b71b51dadf3f21176985a299
Vishnu KS <yo@vishnuks.com>
2019-12-23 15:46:16 +0530

    Install curl and python3.7.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ echo "Hello $1"
 echo "$(ls)"
 echo "Hello again"
 time=$(date)
+apt-get install curl
+apt-get install python3.7-dev
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python3 get-pip.py --user
 pip3 install virtualenv
-virtualenv -p python3.6 .
+virtualenv -p python3.7 .
 source bin/activate
 pip3 install zulip
 echo ::set-output name=time::$time
