698eb811c575af63a362ee56e850a544f94ad008
Vishnu KS <yo@vishnuks.com>
2019-12-24 16:22:56 +0530

    Store email as well in zuliprc.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ pip3 install zulip
 
 site_url=$1
 api_key=$2
+email=$3
 
 mkdir -p archive
 mkdir -p zulip_json
@@ -20,6 +21,7 @@ cp default_settings.py settings.py
 
 crudini --set zuliprc api site $site_url
 crudini --set zuliprc api key $api_key
+crudini --set zuliprc api email $email
 
 export PROD_ARCHIVE=true
 export SITE_URL=$site_url
@@ -29,6 +31,10 @@ python3 archive.py -t
 python3 archive.py -b
 
 cd ..
+
+git config --global user.email "zulip-archive-bot@users.noreply.github.com"
+git config --global user.name "Archive Bot"
+
 git add archive
 git add zulip_json
 git commit -m "Update archive."
